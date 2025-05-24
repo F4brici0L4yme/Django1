1f915f26b5bdbfb8c7ecfff671a184c221bb84eb
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-05-23 23:28:29 -0500

    modelo Personas actualizado con atributos blanck y default

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=100)
-    edad = models.TextField(max_length=3)
+    nombres = models.CharField(max_length=100, blank=False)
+    apellidos = models.CharField(max_length=100, blank=False)
+    edad = models.CharField(max_length=3, blank=False)
+    donador = models.BooleanField(default=False, blank=False)
